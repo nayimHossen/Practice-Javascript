90b9e76eae24eeed9b12d144e08d0c4f4a4ea180
nayimHossen <nayimh2015@gmail.com>
2023-08-10 01:06:37 +0600

    dada type and moduler/remainder operator

---
+++ module-17/remainder.js
@@ -0,0 +1,5 @@
+var mongoes = 81;
+var person = 2;
+
+var remainder = mongoes % person;
+console.log(remainder);
---
+++ module-17/types.js
@@ -0,0 +1,21 @@
+// var price = 100;
+// // console.log(price);
+// var price = "100";
+
+// console.log(typeof price);
+
+// var isHappy = true;
+// // console.log(typeof isHappy);
+
+// var romantics;
+// // console.log(typeof romantics);
+
+// var num1 = 0.1;
+// var num2 = 0.2;
+
+// var sum = num1 + num2;
+
+// sum = sum.toFixed(2);
+
+// sum = parseFloat(sum);
+// console.log(sum);
