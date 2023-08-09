398fba1d0576b64bf7647a651df25d9e3c224e0a
nayimHossen <nayimh2015@gmail.com>
2023-08-10 00:29:27 +0600

    variable and variable naming convention

---
+++ module-17/variable-name.js
@@ -0,0 +1,32 @@
+var price = 23;
+
+
+//1. variable name can not be any reserve keyword
+var false = 90;
+var return = true;
+
+
+//2. variable name has to be in one word. No space
+
+var my name is = "nayim"
+
+//3. variable name can not have quotation
+
+var "name" = "tom";
+
+// 4. variable name can not start with a number but can ends with a number
+
+var 87club = 23;
+var club5 = 47;
+
+//5. hot use log name
+
+//can not use dash
+
+var user-name = "Hossen";
+
+var user_name = "hossen"; //snake case
+
+var userHomeAddress = "Dhaka"; // camel case: we will use this
+
+var UserPhoneNumber = "01772936103"; //pascal case
