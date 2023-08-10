a4fce89bd30ab1e1351624e7b3291407d71ede81
nayimHossen <nayimh2015@gmail.com>
2023-08-10 16:36:43 +0600

    forloop, whileloop

---
+++ module-19/forLoop.js
@@ -0,0 +1,30 @@
+// var roastGiven = 0;
+
+// while (roastGiven < 7) {
+//   console.log("rost den");
+//   roastGiven++;
+// }
+
+// for (var roastGiven = 0; roastGiven < 7; roastGiven++) {
+//   console.log("rost, den please");
+// }
+
+//simple version of for loop
+
+// for (var i = 0; i < 7; i++) {
+//   console.log(i);
+// }
+
+// for (var i = 0; i <= 10; i++) {
+//   console.log(i);
+// }
+
+//odd number
+// for (var i = 0; i <= 20; i += 2) {
+//   console.log(i);
+// }
+
+//even numbers
+// for (var i = 1; i <= 20; i += 2) {
+//   console.log(i);
+// }
---
+++ module-19/while.js
@@ -0,0 +1,11 @@
+// var roastGiven = 0;
+
+// while (roastGiven < 7) {
+//   console.log("rost den, please!!");
+//   //   roastGiven = roastGiven + 1
+//   //   roastGiven += roastGiven;
+//   console.log(roastGiven);
+//   roastGiven++;
+// }
+
+//loop variable
