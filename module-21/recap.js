b9f98f77940ce1e37ca38ebe576f98c80f80ee3e
nayimHossen <nayimh2015@gmail.com>
2023-08-11 18:53:20 +0600

    javascript recap

---
+++ module-21/recap.js
@@ -0,0 +1,45 @@
+//1. variable
+var myName = "Khan";
+
+//math operation +, -, *, /
+//shorthand: +=, --, /=
+//++, --
+
+//2. array
+var friends = ["a", "b", "c"];
+
+friends[2];
+friends[1] = "Khan";
+
+//3. conditionals
+//>,<,>=, <=, ==, ===, !==
+
+if (friends.length < 2) {
+  console.log("Hello");
+} else {
+}
+
+//4. loop
+var number = 0;
+while (number <= 6) {
+  console.log(number);
+  number++;
+}
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+//5. function
+
+function add(a, b) {
+  return a + b;
+}
+
+//object
+var nayim = {
+  name: "nayim",
+  phone: "0177293554",
+  gender: "male",
+  isStudent: true,
+};
