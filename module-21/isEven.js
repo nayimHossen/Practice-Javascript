a97d454197da783ac0686952f6cd54da968c4cda
nayimHossen <nayimh2015@gmail.com>
2023-08-11 19:44:51 +0600

    number is odd or even

---
+++ module-21/isEven.js
@@ -0,0 +1,12 @@
+function isEven(number) {
+  if (number % 2 == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const result = isEven(2);
+const result2 = isEven(3);
+console.log(result);
+console.log(result2);
--- module-21/milesToKilometer.js
+++ module-21/milesToKilometer.js
@@ -1,4 +1,4 @@
-function milesToKilometer(miles) {
-  const kilometer = miles * 1.609;
-  return kilometer;
-}
+// function milesToKilometer(miles) {
+//   const kilometer = miles * 1.609;
+//   return kilometer;
+// }
