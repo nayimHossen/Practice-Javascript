dfb9f3711b38f24bc853dbb8ef7b4dbb2dfe90d4
nayimHossen <nayimh2015@gmail.com>
2023-08-13 23:13:27 +0600

    find odd number from array and return a odd number array

---
+++ module-21/findOddNumberAndMakeArray.js
@@ -0,0 +1,14 @@
+// function oddNumbersArray(numbers) {
+//   let oddArray = [];
+//   for (let i = 0; i < array.length; i++) {
+//     const number = array[i];
+//     if (number % 2 !== 0) {
+//       oddArray.push(number);
+//     }
+//   }
+//   return oddArray;
+// }
+
+// const array = [2, 34, 59, 0, 9, 1];
+// const result = oddNumbersArray(array);
+// console.log(result);
