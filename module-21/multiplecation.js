3983dd1a2666f0e74514ec945845b3ffcc01ddb5
nayimHossen <nayimh2015@gmail.com>
2023-08-13 23:27:22 +0600

    factorial

--- module-21/multiplecation.js
+++ module-21/multiplecation.js
@@ -1,10 +1,10 @@
-function sumOfNumbers(number) {
-  let sum = 1;
-  for (let i = 1; i <= number; i++) {
-    sum *= i;
-  }
-  return sum;
-}
+// function sumOfNumbers(number) {
+//   let sum = 1;
+//   for (let i = 1; i <= number; i++) {
+//     sum *= i;
+//   }
+//   return sum;
+// }
 
-const result = sumOfNumbers(7);
-console.log(result);
+// const result = sumOfNumbers(7);
+// console.log(result);
