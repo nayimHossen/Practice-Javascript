8f5fc073915dc04c3539585d5ee0c876629033b9
nayimHossen <nayimh2015@gmail.com>
2023-08-13 23:30:48 +0600

    factorial with while loop

---
+++ module-21/factorialWhileLoop.js
@@ -0,0 +1,13 @@
+// function factorial(number) {
+//   let result = 1;
+//   let i = number;
+//   while (i >= 1) {
+//     result *= i;
+//     i--;
+//   }
+
+//   return result;
+// }
+
+// const result = factorial(5);
+// console.log(result);
