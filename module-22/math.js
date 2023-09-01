1a0f1a8a508f94dfcaacf1fc457abd29dbc32555
nayimHossen <nayimh2015@gmail.com>
2023-09-01 14:57:21 +0600

    javascript math

---
+++ module-22/math.js
@@ -0,0 +1,23 @@
+// const result = Math.pow(3, 8);
+// console.log(result);
+
+// const num1 = 25;
+// const num2 = 45;
+
+// const gap = Math.abs(num1 - num2);
+
+// if (gap < 5) {
+//   console.log("friend");
+// } else {
+//   console.log("Not possible");
+// }
+
+// const number = 2.3948;
+
+// const fullName = Math.round(number);
+// // console.log(fullName);
+
+// const ceilNumber = Math.ceil(number);
+// console.log(ceilNumber);
+
+// console.log(Math.random());
