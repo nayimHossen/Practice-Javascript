cdc40aa46dab10ecf4b1437074b98a5bf9154788
nayimHossen <nayimh2015@gmail.com>
2023-09-01 14:45:34 +0600

    split, join, slice, concate

---
+++ module-22/split.concat.js
@@ -0,0 +1,23 @@
+// const lyrics = "tomi bondhu kala pakhi";
+
+// // // const parts = lyrics.split(" ");
+// // // console.log(parts);
+
+// // const sentences = lyrics.split(" : ");
+// // console.log(sentences);
+
+// // const partial = lyrics.slice(1, 3);
+
+// // console.log(partial);
+
+// // const partial = lyrics.substring(5, 8);
+// // console.log(partial);
+
+// const lines = [
+//   "tomi bondhu kala pakhi ami jeno ki",
+//   "bosonto kale ame bolete parini ",
+//   "Kala kala sada sada",
+// ];
+
+// const newSongs = lines.join(":");
+// console.log(newSongs);
