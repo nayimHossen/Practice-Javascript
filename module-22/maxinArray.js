a4a7b50161a1dd5e8f03d0cef971d18ca9848287
nayimHossen <nayimh2015@gmail.com>
2023-09-01 16:02:03 +0600

    maxNumberInArray

---
+++ module-22/maxinArray.js
@@ -0,0 +1,16 @@
+// const numbers = [23, 59, 39, 12];
+
+// function maxInArray(numbers) {
+//   let largest = numbers[0];
+
+//   for (let i = 0; i < numbers.length; i++) {
+//     const number = numbers[i];
+//     if (number > largest) {
+//       largest = number;
+//     }
+//   }
+//   return largest;
+// }
+
+// const tallest = maxInArray(numbers);
+// console.log(tallest);
