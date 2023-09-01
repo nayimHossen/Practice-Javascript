a13351c3bc9b1a67e15995fd9e75379c73486b79
nayimHossen <nayimh2015@gmail.com>
2023-09-01 15:13:24 +0600

    variable swap

---
+++ module-22/swap.variable.js
@@ -0,0 +1,14 @@
+// let first = 5;
+// let second = 10;
+
+// console.log(first, second);
+
+// // first = second;
+// // second = first;
+
+// let temp = first;
+
+// first = second;
+// second = temp;
+
+// console.log(first, second);
