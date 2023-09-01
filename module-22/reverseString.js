9b5d988252fa76e9140bde242960bd142326c8c7
nayimHossen <nayimh2015@gmail.com>
2023-09-01 16:30:52 +0600

    words reversed

---
+++ module-22/reverseString.js
@@ -0,0 +1,11 @@
+// function reverseString(text) {
+//   let reversed = "";
+//   for (let i = text.length - 1; i >= 0; i--) {
+//     const element = text[i];
+//     reversed += element;
+//   }
+//   return reversed;
+// }
+
+// const result = reverseString("Hello");
+// console.log(result);
---
+++ module-22/wordReverse.js
@@ -0,0 +1,13 @@
+// function reverseWords(str) {
+//   const words = str.split(" ");
+//   let result = [];
+//   for (let i = words.length - 1; i >= 0; i--) {
+//     const element = words[i];
+//     result.push(element);
+//   }
+//   const reversed = result.join(" ");
+//   return reversed;
+// }
+
+// const result = reverseWords("I love you");
+// console.log(result);
