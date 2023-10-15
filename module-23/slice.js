b6918b08994473eb8880eaebd58bfee32af998ae
nayimHossen <nayimh2015@gmail.com>
2023-10-16 01:05:50 +0600

    slice and splice

---
+++ module-23/slice.js
@@ -0,0 +1,7 @@
+// const friends = [12, 45, 5, 64, 5];
+
+// //array index start from 0
+
+// const partial = friends.slice(2, 5);
+// console.log(partial);
+// console.log(friends);
---
+++ module-23/splice.js
@@ -0,0 +1,6 @@
+// const friends = [12, 45, 5, 64, 5, 40, 20];
+
+// const partial = friends.splice(2, 3, 99, 555, 877);
+// console.log(partial);
+
+// console.log(friends);
