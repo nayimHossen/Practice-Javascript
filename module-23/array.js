92d713205dd2f23e315798d8dd6e92528dcd0cd6
nayimHossen <nayimh2015@gmail.com>
2023-10-16 00:55:52 +0600

    isArray, concat, includes

---
+++ module-23/array.js
@@ -0,0 +1,32 @@
+// const country = "Bangladesh";
+// const age = 23;
+// const isIndependent = true;
+// const student = { id: 232, class: 11, name: "Hossen" };
+// const friends = [2, 3, 4, 56, 8];
+
+// function add(n1, n2) {
+//   return n1 + n2;
+// }
+
+// console.log(typeof country);
+// console.log(typeof age);
+// console.log(typeof isIndependent);
+// console.log(typeof student);
+
+// //when you want to check is array or not
+// console.log(Array.isArray(friends));
+// console.log(typeof add);
+
+// //when you want to check any element includes or not in array
+
+// console.log(friends.includes(2));
+
+// if (friends.indexOf(18) !== 1) {
+// }
+
+// //concat
+
+// const newFriendsAge = [23, 34, 5, 6];
+
+// const allFriends = newFriendsAge.concat(friends);
+// console.log(allFriends);
