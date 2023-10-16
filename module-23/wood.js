341c690aef8ef1be3aef0558984a1001ee107dd3
nayimHossen <nayimh2015@gmail.com>
2023-10-16 09:15:36 +0600

    wood calculator

---
+++ module-23/wood.js
@@ -0,0 +1,22 @@
+// /*
+// 1. chair --> 3 cft
+// 2. table --> 10 cft
+// 3. bed --> 50 cft
+// */
+
+// function woodCalculator(chairQuantity, tableQuantity, bedQuantity) {
+//   const perChairWood = 3;
+//   const perTableWood = 10;
+//   const perBedWood = 50;
+
+//   const chairWood = chairQuantity * perChairWood;
+//   const tableWood = tableQuantity * perTableWood;
+//   const bedWood = bedQuantity * perBedWood;
+
+//   return chairWood + tableWood + bedWood;
+
+//   //   console.log(chairQuantity, tableQuantity, bedQuantity);
+// }
+
+// const totalWood = woodCalculator(2, 3, 4);
+// console.log(totalWood);
