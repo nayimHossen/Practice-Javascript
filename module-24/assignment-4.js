d8ce56f3350853586956c40cb6dca6856a310aa7
nayimHossen <nayimh2015@gmail.com>
2023-11-02 14:06:38 +0600

    assignment task

---
+++ module-24/assignment-4.js
@@ -0,0 +1,27 @@
+//1. convert radian to degree
+radianToDegree;
+
+//2. check whether the given file name is a javascript file or not
+isJavascriptFile;
+
+//3.calculate the total oil price that I have to pay
+/*
+    diesel ---> 114
+    petrol ---> 130
+    octane ---> 135
+*/
+
+oilPrice;
+
+//4.
+
+/*
+    reserved bus ---> 50
+    microbus ---> 11
+    rest people will go by public bus
+    people: 65
+    bus remaining ---> 15
+    microbus ---> 4
+    public bus fare ---> 250
+*/
+publicBusFare;
