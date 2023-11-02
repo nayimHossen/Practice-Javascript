e887094effbae0ad7bd7391914bcf085a55b6048
nayimHossen <nayimh2015@gmail.com>
2023-11-02 14:52:53 +0600

    problem-3 and problem-5 solved

---
+++ module-24/problem-3.js
@@ -0,0 +1,15 @@
+// function oilPrice(diesel, petrol, octane) {
+//   const dieselPrice = 114;
+//   const petrolPrice = 130;
+//   const octanePrice = 135;
+
+//   const dieselCost = diesel * dieselPrice;
+//   const petrolCost = petrol * petrolPrice;
+//   const octaneCost = octane * octanePrice;
+
+//   const totalCost = dieselCost + petrolCost + octaneCost;
+//   return totalCost;
+// }
+
+// const result = oilPrice(2, 3, 4);
+// console.log(result);
---
+++ module-24/problem-5.js
@@ -0,0 +1,13 @@
+// function isBestFriend(man1, man2) {
+//   if (man1.name === man2.friend && man2.name === man1.friend) {
+//     return `Friend`;
+//   } else {
+//     return `Friend`;
+//   }
+// }
+
+// const result = isBestFriend(
+//   { name: "Tom", friend: "Rock" },
+//   { name: "Rock", friend: "Tom" }
+// );
+// console.log(result);
