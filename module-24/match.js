04615584ae91c4070e1fb78e6868ba8b5cf226d1
nayimHossen <nayimh2015@gmail.com>
2023-11-03 23:23:34 +0600

    search from array with for of loop

---
+++ module-24/match.js
@@ -0,0 +1,35 @@
+// // const numbers = [2, 3, 45, 6, 4];
+
+// // // for (let i = 0; i < numbers.length; i++) {
+// // //   const number = numbers[i];
+// // //   console.log(number);
+// // // }
+
+// // for (const number of numbers) {
+// //   console.log(number);
+// // }
+
+// const products = [
+//   { id: 1, name: "walton phone", price: 5654 },
+//   { id: 2, name: "samsung", price: 54564 },
+//   { id: 3, name: "I phone", price: 20000 },
+//   { id: 4, name: "vivo phone", price: 20000 },
+//   { id: 5, name: "oppo", price: 946543 },
+//   { id: 6, name: "symphony phone", price: 53641 },
+//   { id: 7, name: "okpia phone", price: 54554 },
+//   { id: 8, name: "normal phone", price: 13243 },
+//   { id: 8, name: "laptop phone", price: 13243 },
+// ];
+
+// function matchProducts(products, search) {
+//   const matched = [];
+//   for (const product of products) {
+//     if (product.name.toLowerCase().includes(search.toLowerCase())) {
+//       matched.push(product);
+//     }
+//   }
+//   return matched;
+// }
+
+// const result = matchProducts(products, "laptop");
+// console.log(result);
