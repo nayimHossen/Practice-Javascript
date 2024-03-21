5ec8a298f3dd5f309ea15865dc7b92c4a738051b
nayimHossen <nayimh2015@gmail.com>
2024-03-21 14:13:25 +0600

    setAttribute, remoteAttribute, key events

--- moduel-26/js/events.js
+++ moduel-26/js/events.js
@@ -1,3 +1,11 @@
-document.getElementById("tomato", function () {
-  document.body.style.backgroundColor = "red";
+// document.getElementById("btn-more").addEventListener("click", function () {
+//   console.log("clicked");
+// });
+
+// document.getElementById("text-filed").addEventListener("focus", function () {
+//   console.log("clicked");
+// });
+
+document.getElementById("text-filed").addEventListener("keyup", function (e) {
+  console.log(e.target.value);
 });
